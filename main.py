002e44ba9af73d6bf1cd1827d43fbcc04799ee85
XinhaoLiu <89992445+Danielyqss@users.noreply.github.com>
2022-01-23 23:44:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,204 @@
+point = 0
+
+def dijin(point):
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True:  
+        times= input("PICK YOU CHOICE ")
+        if times == "A":
+            point += 1
+            print(point)
+
+        elif times == "B":
+            point += 2
+            print(point)
+
+        elif times == "C":
+            point += 3
+            print(point)
+        break
+        print("\n")
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True: 
+          times= input("PICK YOU CHOICE ")
+          if times == "A":
+              point += 1
+              print(point)  
+          elif times == "B":
+              point += 2
+              print(point)
+
+          elif times == "C":
+              point += 3
+              print(point)
+          break 
+          print("\n")
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True: 
+          times= input("PICK YOU CHOICE ")
+          if times == "A":
+              point += 1
+              print(point)  
+          elif times == "B":
+              point += 2
+              print(point)
+
+          elif times == "C":
+              point += 3
+              print(point)
+          break 
+          print("\n")
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True: 
+          times= input("PICK YOU CHOICE ")
+          if times == "A":
+              point += 1
+              print(point)  
+          elif times == "B":
+              point += 2
+              print(point)
+
+          elif times == "C":
+              point += 3
+              print(point)
+          break 
+          print("\n")
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True: 
+          times= input("PICK YOU CHOICE ")
+          if times == "A":
+              point += 1
+              print(point)  
+          elif times == "B":
+              point += 2
+              print(point)
+
+          elif times == "C":
+              point += 3
+              print(point)
+          break 
+          print("\n")
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True: 
+          times= input("PICK YOU CHOICE ")
+          if times == "A":
+              point += 1
+              print(point)  
+          elif times == "B":
+              point += 2
+              print(point)
+
+          elif times == "C":
+              point += 3
+              print(point)
+          break 
+          print("\n")
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True: 
+          times= input("PICK YOU CHOICE ")
+          if times == "A":
+              point += 1
+              print(point)  
+          elif times == "B":
+              point += 2
+              print(point)
+
+          elif times == "C":
+              point += 3
+              print(point)
+          break 
+          print("\n")
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True: 
+          times= input("PICK YOU CHOICE ")
+          if times == "A":
+              point += 1
+              print(point)  
+          elif times == "B":
+              point += 2
+              print(point)
+
+          elif times == "C":
+              point += 3
+              print(point)
+          break 
+          print("\n")
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True: 
+          times= input("PICK YOU CHOICE ")
+          if times == "A":
+              point += 1
+              print(point)  
+          elif times == "B":
+              point += 2
+              print(point)
+
+          elif times == "C":
+              point += 3
+              print(point)
+          break 
+          print("\n")
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True: 
+          times= input("PICK YOU CHOICE ")
+          if times == "A":
+              point += 1
+              print(point)  
+          elif times == "B":
+              point += 2
+              print(point)
+
+          elif times == "C":
+              point += 3
+              print(point)
+          break 
+          print("\n")
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True: 
+          times= input("PICK YOU CHOICE ")
+          if times == "A":
+              point += 1
+              print(point)  
+          elif times == "B":
+              point += 2
+              print(point)
+
+          elif times == "C":
+              point += 3
+              print(point)
+          break 
+          print("\n")
+  print("how are you?")      
+  print("A, haha B, LOL      C,DEM")
+  while True: 
+          times= input("PICK YOU CHOICE ")
+          if times == "A":
+              point += 1
+              print(point)  
+          elif times == "B":
+              point += 2
+              print(point)
+
+          elif times == "C":
+              point += 3
+              print(point)
+          return point
+          break 
+          print("\n")
+
+
+
+PRINT 
+
+dijin(point)
+feed(point)
---
+++ s.md
@@ -0,0 +1,2 @@
+return point
+print(point)
